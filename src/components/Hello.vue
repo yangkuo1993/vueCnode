1f279702480e08f4c6987fa30034975305a472a7
yangkuo <734271234@qq.com>
2017-08-23 16:42:12 +0800

    axios

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -27,6 +27,13 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted () {
+    this.$http.get('https://cnodejs.org/api/v1/topics').then((data) => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 }
 </script>
